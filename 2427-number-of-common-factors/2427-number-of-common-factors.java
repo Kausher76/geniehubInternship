7ddda9154f82e3db1c1d40d803bd44bb56d5695d
Kausher Imam <86553740+Kausher76@users.noreply.github.com>
2024-01-06 16:00:48 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (36.08%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,12 +1,12 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
+       int count = 0;
         int min = (a > b) ? b : a;
         for(int i = 1; i < (min + 1); i++) {
             if((a % i) == 0 && (b % i) == 0 ) {
-                arrlist.add(i);
+                count  += 1;
             }
         }
-        return arrlist.size();
+        return count;
     }
 }
