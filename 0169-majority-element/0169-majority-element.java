039a5c7af6ce6e0a17e8351d0610d10ba50a74e4
Kausher Imam <86553740+Kausher76@users.noreply.github.com>
2024-01-14 18:19:36 +0530

    Time: 3 ms (62.92%), Space: 50.6 MB (15.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        Map<Integer, Integer> hash = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            hash.put(nums[i], hash.getOrDefault(nums[i], 0) + 1);
-        }
-
-        int maxCount = 0;
-        int majorityElement = 0;
-
-        for (Map.Entry<Integer, Integer> entry : hash.entrySet()) {
-            int currentCount = entry.getValue();
-
-            if (currentCount > maxCount) {
-                maxCount = currentCount;
-                majorityElement = entry.getKey();
-            }
-        }
-
-        return majorityElement;
+        return nums[n/2];
     }
 }
